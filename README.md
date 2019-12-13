7dd2448512f0a68075cfded4c95a450e55e78165
Yi-Hsiu, Hsu <yihsiu.hsu@sifive.com>
2019-12-12 18:12:03 -0800

    Init

---
+++ README.md
@@ -0,0 +1,2 @@
+# hellocpp
+A simple "Hello, World!" example to demonstrate printf and build environment for C++ program.
---
+++ hellocpp.cpp
@@ -0,0 +1,14 @@
+/* Copyright 2019 SiFive, Inc */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#include <iostream>
+#include <stdio.h>
+void *__dso_handle;
+
+using namespace std; 
+
+int main() { 
+  cout << "Hello C++ !\n";
+//  printf("Hello C++ from printf\n");
+ return 0; 
+}
